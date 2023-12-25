dc2b5b54a0ec17f381f0cf18bda930c85879e462
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2023-12-24 21:34:53 -0400

    Styles, Fonts and Images, Chapter 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <h1 className='text-blue-500'>Titulo principal</h1>
+        <div style={{ border: '1px solid #000' }}>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
